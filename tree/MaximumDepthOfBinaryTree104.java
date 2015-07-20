a09e6d3dc00c9b58d6995ea610f5379a983e1a68
qiang2010 <qiangji1991@gmail.com>
2015-07-20 09:49:40 +0800

    leetcode qiang

--- tree/MaximumDepthOfBinaryTree104.java
+++ tree/MaximumDepthOfBinaryTree104.java
@@ -1,4 +1,4 @@
-package qiang.leetcode;
+package qiang.tree;
 
 public class MaximumDepthOfBinaryTree104 {
 
@@ -20,12 +20,12 @@ public class MaximumDepthOfBinaryTree104 {
 	* }
 	*/
 
-//	    public int maxDepth(TreeNode root) {
-//	        if(root == null) return 0;
-//	        int left = maxDepth(root.left);
-//	        int right = maxDepth(root.right);
-//	        if(left > right) return left+1;
-//	        else return right+1;
-//	    
-//	   }
+	    public int maxDepth(TreeNode root) {
+	        if(root == null) return 0;
+	        int left = maxDepth(root.left);
+	        int right = maxDepth(root.right);
+	        if(left > right) return left+1;
+	        else return right+1;
+	    
+	   }
 }
