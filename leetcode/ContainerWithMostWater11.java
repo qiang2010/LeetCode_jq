014bca5ab03c449b5427f748f976aa353c650f0d
tangbo <tangboneu@foxmail.com>
2015-08-10 20:58:17 +0800

    jq

---
+++ leetcode/ContainerWithMostWater11.java
@@ -0,0 +1,20 @@
+package qiang.leetcode;
+
+/**
+ * Given n non-negative integers a1, a2, ..., an, 
+ * where each represents a point at coordinate (i, ai). 
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, 
+ * such that the container contains the most waterNote: You may not slant the container.
+ * @author qiang
+ *
+ */
+public class ContainerWithMostWater11 {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
